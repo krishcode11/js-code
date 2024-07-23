4091768e1dbdd8d12141cdde6021cf3070eed28e
krishcode11 <shaneraval239@gmail.com>
2024-07-23 19:12:17 +0530

    02_dataType.js configure done

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -2,6 +2,13 @@ const accountId = 112231
 let accountemail = "saheve@gmail.com"
 var accountpassword = "1234"
 accountcity = "jaipur"
+let accountstate;
+
+accountemail = "japsk@gmail.com"
+accountid = "issacc"
+accountcity = "Delhi"
 
 
 console.log(accountId);
+
+console.table({accountemail, accountid, accountcity, accountstate})
