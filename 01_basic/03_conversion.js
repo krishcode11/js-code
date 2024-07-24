8a1155b732903ca47f40d7ef3d52e61916e99696
krishcode11 <shaneraval239@gmail.com>
2024-07-24 20:37:29 +0530

    conversion config done

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,22 @@
+//let num1, num2, num3
+
+//num1 = num2 = num3 = 2 + 2
+
+//let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+//let x = 3;
+//const y = x++;
+
+//console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+//let a = 3;
+//const b = ++a;
+
+//console.log(`a:${a}, b:${b}`)
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
