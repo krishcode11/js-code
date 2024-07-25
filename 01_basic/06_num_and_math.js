838ac0b72bfcebf23db22848b362b82add13198d
krishcode11 <shaneraval239@gmail.com>
2024-07-25 17:15:13 +0000

    math and num

---
+++ 01_basic/06_num_and_math.js
@@ -0,0 +1,35 @@
+const score = 400 
+
+const balance = new Number (100)
+//console.log(balance);
+
+//console.log(balance.toString().length)
+    //console.log(balance.toFixed(0.1))
+
+const otherNumber = 23.8923
+
+// console.log(otherNumber.toPrecision(2))
+
+const hundreds = 100000
+//console.log(hundreds.toLocaleString('en-In'))
+
+///////////////////Maths////////////////////
+
+// console.log(Math)
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(100,3423,2321))
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1 ) 
+console.log(Math.floor(Math.random()*10) + 1 ) 
+
+
+
+const min = 100
+const max = 200
+
+console.log(Math.floor(Math.random() * (max - min + 1 ))+ min) 
+
