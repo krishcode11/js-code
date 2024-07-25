a2efa7faa12e29ea07cec066de57c58757e98173
krishcode11 <shaneraval239@gmail.com>
2024-07-25 14:43:33 +0000

    datatype config done

---
+++ 01_basic/datatype_summary.js
@@ -0,0 +1,35 @@
+// Primitive
+
+// 7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+//const bigNumber = 23456713578n
+
+
+
+// Reference(Non Primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myoj = {
+    name: "krish"
+}
+
+const myfunction = function(){
+    console.log("hellow world");
+}
+
+
+console.log(typeof scoreValue);
