04ce9d30743c48ed6a33679b498eaee8cbf0b5d3
krishcode11 <shaneraval239@gmail.com>
2024-07-26 09:44:36 +0000

    array config done

---
+++ 01_basic/07_date_time.js
@@ -0,0 +1,12 @@
+// Dates
+
+let myDate = new Date ()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+//let myCreatedDate = new Date(3120,0, 23)
+//let myCreatedDate = new Date("2011-02-12")
+let myCreatedDate = new Date("05-02-2023")
+console.log(myCreatedDate.toDateString())
