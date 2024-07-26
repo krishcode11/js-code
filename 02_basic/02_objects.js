ce5c4be5df9b1405d9c776b5b073da157af1241a
krishcode11 <shaneraval239@gmail.com>
2024-07-26 15:46:50 +0000

    object configue done

---
+++ 02_basic/02_objects.js
@@ -0,0 +1,35 @@
+//singleton 
+
+// //object literals 
+
+// const mySym = Symbol("key1")
+
+// const JsUser = {
+//     name: "krish",
+//     [mySym]: "mykey1",
+//     age: 21,
+//     location: "delhi",
+//     email: "krish@gmail.com",
+//     isLoggedIn: false,
+//     lastloginDays: ["Monday", "Saturday"]    
+// }
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser[mySym])
+
+
+// JsUser.email = "krish@gmail.com"
+// // Object.freeze(JsUser);
+// console.log(JsUser)
+
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
