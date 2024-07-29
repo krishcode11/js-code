c6c4acf18871cbc41ad8ad3f3fb2dc63e4e8009c
krishcode11 <shaneraval239@gmail.com>
2024-07-29 11:29:36 +0000

    function config done

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -26,4 +26,35 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("krish"))
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+function calculatecartPrice(val1, val2, ...num1){
+    return num1
+
+}
+
+// console.log(calculatecartPrice(200, 3300, 4000 ))
+
+const user = {
+    username: "krish",
+    price: 122
+}
+function handleObject(anyobject){
+    console.log(`Udername is ${anyobject.username} and price is $ {anyobject.price}` )
+}
+
+// handleObject(user)
+handleObject({
+    username: "saca",
+   price: 322
+})
+
+const myNewArray = [200, 332, 211]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 400, 500, 1000]));
