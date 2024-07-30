ba0abd1944629c1e59891615b909b93655f9315b
krishcode11 <shaneraval239@gmail.com>
2024-07-30 11:04:34 +0000

    scope config done

--- 03_basic/02_scope.js
+++ 03_basic/02_scope.js
@@ -13,3 +13,46 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
+
+function one(){
+    const username = "krish"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "kish"
+    if (username === "kish") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
